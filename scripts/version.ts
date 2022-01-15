615419de8bca46493e069c135ceb7964323f2aa8
tjkoury <tj@digitalarsenal.io>
2022-01-15 09:17:27 -0500

    update to 1.89

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,6 +1,6 @@
-import { writeFileSync } from "fs";
-import cPJ from "../cesium/package.json";
-import packageJSON from '../package.json';
+import { readFileSync, writeFileSync } from "fs";
+const cPJ = JSON.parse(readFileSync("./cesium/package.json", "utf-8"));
+const packageJSON = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 packageJSON.version = cPJ.version;
 writeFileSync("package.json", JSON.stringify(packageJSON, null, 4));
